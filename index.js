78e6b5e67a20813b268e09017a69e6eafeabc2ec
sijan joshi <joshisijan96@gmail.com>
2020-09-23 22:18:38 +0545

    edited

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const path = require('path')
+
+const app = express()
+
+//port for server
+const PORT = process.env.PORT || 5000
+
+//Initializing Middlewares
+//for json
+app.use(express.json());
+
+//Use Router
+app.use('/api', require('./routes/api/nepse'))
+
+
+//listning to port
+app.listen(PORT, () => {
+  console.log(`Server started at post: ${PORT}`);
+})
+
---
+++ routes/api/nepse.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const puppeteer = require("puppeteer");
+const $ = require("cheerio");
+const request = require("request");
+
+const router = express.Router();
+
+// nepse api urls
+const companiesUrl = "https://newweb.nepalstock.com/api/nots/company/list";
+const marketStatusUrl = "https://newweb.nepalstock.com/api/nots/nepse-data/market-open";
+
+//get list of all companies
+router.get("/companies", async (req, res) => {
+  try {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto(companiesUrl);
+    const html = await page.content();
+    res.json(JSON.parse($("pre", html).text()));
+    await browser.close();
+  }catch(err){
+    res.status(400).json({
+      msg: err.message.toString()
+    });
+  }
+});
+
+//get market status
+
+router.get("/status", async (req, res) => {
+  try {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto(marketStatusUrl);
+    const html = await page.content();
+    res.json(JSON.parse($("pre", html).text()));
+    await browser.close();
+  }catch(err){
+    res.status(400).json({
+      msg: err.message.toString()
+    });
+  }
+});
+
+module.exports = router;
