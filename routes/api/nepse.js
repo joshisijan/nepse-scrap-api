3d3dbbc4eba14a3efffaf668930be0398ee804c0
sijan joshi <joshisijan96@gmail.com>
2020-09-24 16:46:34 +0545

    edited

--- routes/api/nepse.js
+++ routes/api/nepse.js
@@ -7,39 +7,40 @@ const router = express.Router();
 
 // nepse api urls
 const companiesUrl = "https://newweb.nepalstock.com/api/nots/company/list";
-const marketStatusUrl = "https://newweb.nepalstock.com/api/nots/nepse-data/market-open";
+const marketStatusUrl =
+  "https://newweb.nepalstock.com/api/nots/nepse-data/market-open";
+const nepseIndexUrl = "https://newweb.nepalstock.com/api/nots/nepse-index";
+const nepseTimeValueUrl = "https://newweb.nepalstock.com/api/nots/graph/index";
 
+// function to make router
+const makeRouter = (routeName, url) => {
+  router.get(routeName, async (req, res) => {
+    try {
+      const browser = await puppeteer.launch({ args: ["--no-sandbox"] });
+      const page = await browser.newPage();
+      if(req.params.id){
+        await page.goto(`${url}/${req.params.id}`);
+      }else{
+        await page.goto(url);
+      }
+      const html = await page.content();
+      res.json(JSON.parse($("pre", html).text()));
+      await browser.close();
+    } catch (err) {
+      res.status(400).json({
+        msg: err.message.toString(),
+      });
+    }
+  });
+};
+
+// get market status
+makeRouter("/status", marketStatusUrl);
+//get nepse index
+makeRouter("/nepse-index", nepseIndexUrl);
+//get time and value of nepse
+makeRouter("/nepse-chart/:id", nepseTimeValueUrl);
 //get list of all companies
-router.get("/companies", async (req, res) => {
-  try {
-    const browser = await puppeteer.launch({ args: ['--no-sandbox'] });
-    const page = await browser.newPage();
-    await page.goto(companiesUrl);
-    const html = await page.content();
-    res.json(JSON.parse($("pre", html).text()));
-    await browser.close();
-  }catch(err){
-    res.status(400).json({
-      msg: err.message.toString()
-    });
-  }
-});
-
-//get market status
-
-router.get("/status", async (req, res) => {
-  try {
-    const browser = await puppeteer.launch({ args: ['--no-sandbox'] });
-    const page = await browser.newPage();
-    await page.goto(marketStatusUrl);
-    const html = await page.content();
-    res.json(JSON.parse($("pre", html).text()));
-    await browser.close();
-  }catch(err){
-    res.status(400).json({
-      msg: err.message.toString()
-    });
-  }
-});
+makeRouter("/companies", companiesUrl);
 
 module.exports = router;
